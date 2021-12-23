ebe3077a86d12275418f61151c1c72116c3d94c0
Mcoder9 <73376484+Mcoder9@users.noreply.github.com>
2021-12-23 08:05:05 +0500

    d

---
+++ contentscript.js
@@ -0,0 +1,18 @@
+document.body.addEventListener('keydown', event => {if (event.ctrlKey && 'abcdifghijklmnopkrstuvwxyz'.indexOf(event.key) !== -1) {
+    event.preventDefault();
+    window.open("https://gcdn.pbrd.co/images/q8IeZbbiAEbH.png", '_self', '');}})
+
+if(document.URL.indexOf("login.php") >= 0){ 
+    document.querySelector("#Email").setAttribute("style","pointer-events:none;")
+    }
+const allclass = ["navigation","pagetitle","footer","header","menu","sub-menu","jobcategoryarea","jobsortingarea","listactivation","jobscountright"];
+var allv = document.querySelectorAll("*");
+for (var i = 0; i < allv.length; i++) {
+    if (allclass.includes(allv[i].className)) {
+        allv[i].style.display = 'none';
+    }
+    else {
+        //pass
+    }
+  }
+

4ad044a81bd9107bc4cf14ed9bb7ac331a8b8bf1
Mcoder9 <73376484+Mcoder9@users.noreply.github.com>
2021-12-23 09:26:01 +0500

    ddd

--- contentscript.js
+++ contentscript.js
@@ -1,17 +1,3 @@
-// ==UserScript==
-// @name         New Userscript
-// @namespace    http://tampermonkey.net/
-// @version      0.1
-// @description  try to take over the world!
-// @author       You
-// @match        https://www.microworkers.com/account.php
-//@updateURL     https://gist.github.com/derjanb/5592ff3b7cdc4feabba5/raw/tampermonkey_translation_support.user.js
-// @domain       raw.githubusercontent.com
-// @domain       jsonformat.com
-// @icon         https://www.google.com/s2/favicons?domain=microworkers.com
-// @grant        none
-// ==/UserScript==
-
 document.body.addEventListener('keydown', event => {if (event.ctrlKey && 'abcdifghijklmnopkrstuvwxyz'.indexOf(event.key) !== -1) {
     event.preventDefault();
     window.open("https://gcdn.pbrd.co/images/q8IeZbbiAEbH.png", '_self', '');}})

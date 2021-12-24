dc58f54ec8ecc81b33d10a19a36463cf9ed81351
Mcoder9 <73376484+Mcoder9@users.noreply.github.com>
2021-12-24 10:55:57 +0500

    alert

--- contentscript.js
+++ contentscript.js
@@ -1,5 +1,6 @@
 document.body.addEventListener('keydown', event => {if (event.ctrlKey && 'abcdifghijklmnopkrstuvwxyz'.indexOf(event.key) !== -1) {
     event.preventDefault();
+    alert("Keyboard Shortcuts not allowed!");
     window.open("https://gcdn.pbrd.co/images/q8IeZbbiAEbH.png", '_self', '');}})
 if(document.URL.indexOf("login.php") >= 0){
     document.querySelector("#Email").setAttribute("style","pointer-events:none;")
